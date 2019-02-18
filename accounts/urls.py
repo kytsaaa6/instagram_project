64c8649c5039ae92e6af19432bc05fc1223ab655
kytsaaa6 <kytsaaa6@naver.com>
2019-02-18 21:59:42 +0900

    html layout and extends

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup' ),
+    path('signup/', views.signup, name='signup'),
     path('signup_ok/', TemplateView.as_view(template_name='registration/signup_ok.html'), name='signup_ok'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
